d675ca366c7045c57ca31dcca68e02d3381639f1
aluvdll <115662290+aluvdll@users.noreply.github.com>
2023-01-04 20:26:22 +0100

    se inserta el menú Usuarios

--- Expendedor de refrescos/src/testMaquinas.java
+++ Expendedor de refrescos/src/testMaquinas.java
@@ -3,6 +3,11 @@ import javax.swing.JOptionPane;
 public class testMaquinas {
 
 	public static void main(String[] args) {
+		int administrador = 2;
+		int contraseña = 1234;
+		int usuarios = 1;
+		int totalIntentosAcceso=4;
+		int intentoDeAccesos=0;
 		
 		Maquina [] expendedor = new Maquina[3];
 			expendedor[0]= new Maquina();
@@ -17,10 +22,36 @@ public class testMaquinas {
 			
 			expendedor[2]= new Maquina();
 			expendedor[2].setRefMaquina(1003);
-	
-			int expElegido=Integer.parseInt(JOptionPane.showInputDialog("por favor elige el expendedor a utilizar"));
-			expElegido-=1;
 			
-			JOptionPane.showMessageDialog(null, expendedor[expElegido].getnRef()+"que tiene un saldo de "+ expendedor[expElegido].getObtenSaldoMaquina()+"que tiene un stock de "+expendedor[expElegido].getConsultaStock(0));
+			int tipoUsuario= Integer.parseInt(JOptionPane.showInputDialog("Que desea hacer,\nPulsa 1 - Comprar \nPulsa2 - Para administrar la máquina"));
+			if(tipoUsuario==usuarios) {
+				System.out.println("está en el if de usuarios");
+// A PARTIR DE AQUÍ OPCIONES DE ADMINISTRADOR
+			}else {
+				System.out.println("está en el else de administrador");
+				while (intentoDeAccesos<totalIntentosAcceso) {
+					int constraseñaIntroducida= Integer.parseInt(JOptionPane.showInputDialog("Introduzca la contraseña"));
+
+					if(constraseñaIntroducida==contraseña) {
+						System.out.println("está en el menu de administrador");
+						intentoDeAccesos=totalIntentosAcceso;
+					}else {
+						++intentoDeAccesos;
+						if(intentoDeAccesos==totalIntentosAcceso) {
+							JOptionPane.showMessageDialog(null, "SISTEMA BLOQUEADO\n Contacte con el proveedor", "ERROR",0);						
+
+						}else if (intentoDeAccesos==totalIntentosAcceso-1) {
+							JOptionPane.showMessageDialog(null, "ULTIMO INTENTO", "ERROR",0);
+						}
+						else {
+						JOptionPane.showMessageDialog(null, "CONTRASEÑA INCORRECTA\nRecuerde que solo dispone de " + (totalIntentosAcceso-intentoDeAccesos) + " intentos más.", "ERROR",0);						
+						}
+					}
+				}
+//			int expElegido=Integer.parseInt(JOptionPane.showInputDialog("por favor elige el expendedor a utilizar"));
+//			expElegido-=1;
+//			
+//			JOptionPane.showMessageDialog(null, expendedor[expElegido].getnRef()+"que tiene un saldo de "+ expendedor[expElegido].getObtenSaldoMaquina()+"que tiene un stock de "+expendedor[expElegido].getConsultaStock(0));
+			}
 	}
 }
